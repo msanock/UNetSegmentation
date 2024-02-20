6cd204005cd7b00b926e4c693d9653fd82825699
Maciej Sanocki <msanock@gmail.com>
2024-02-20 15:15:43 +0100

    installation script for cityscapes dataset

---
+++ install_dataset.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+path_to_raw_dataset='./data'
+
+mkdir -p $path_to_raw_dataset
+
+python -m pip install cityscapesscripts
+
+csDownload  -l
+
+csDownload leftImg8bit_trainvaltest.zip -d $path_to_raw_dataset
+csDownload gtFine_trainvaltest.zip -d $path_to_raw_dataset
+
+unzip $path_to_raw_dataset/leftImg8bit_trainvaltest.zip -d $path_to_raw_dataset
+unzip $path_to_raw_dataset/gtFine_trainvaltest.zip -d $path_to_raw_dataset
+
+rm $path_to_raw_dataset/leftImg8bit_trainvaltest.zip
+rm $path_to_raw_dataset/gtFine_trainvaltest.zip
+
+echo "Cityscapes dataset has been downloaded and extracted to $path_to_raw_dataset"
+
+
